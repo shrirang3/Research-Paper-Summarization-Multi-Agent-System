446bbbbdd8042bc59d6fb6d37628a730277d4b30
Shrirang <shrirang.mahankaliwar21@vit.edu>
2025-05-11 16:04:42 +0530

    Pipeline Testing

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 from agents.search_rp import search_arxiv
 from agents.summary_generator import ArxivSummarizerAgent
+from agents.classifier import ResearchPaperClassifier
 import os
 from dotenv import load_dotenv
 
@@ -7,14 +8,39 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-query = "1706.03762"  # Attention Is All You Need paper
+query = "Deep learning"  # Attention Is All You Need paper
 papers = search_arxiv(query)
 
-#print(papers[0]['title']) We get title of paper
+print(papers[0]['title']) #We get title of paper
 
 if papers:
     summarizer=ArxivSummarizerAgent()
-    first_paper = papers[0]
-    print(summarizer.summarize_paper(first_paper)) # We get summary of paper
+    
+    #print(summarizer.summarize_paper(papers[0])) # We get summary of paper
 else:
     print("No papers found.")
+
+
+# Define your categories
+PAPER_CATEGORIES = [
+    "NLP", 
+    "Computer Vision",
+    "Machine Learning",
+    "Neuroscience",
+    "Robotics"
+]
+
+# Initialize classifier
+
+
+
+classifier = ResearchPaperClassifier(PAPER_CATEGORIES, use_llm=True)
+# Get classification
+result = classifier.classify(
+    title=papers[0]['title'],
+    abstract=papers[0]['abstract']
+)
+
+print("Classification Result:")
+print(result)
+
